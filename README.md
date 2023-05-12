3a4eefbf5caea89579b4142de35223ee7b2ebb20
Matt Born <mattborn@users.noreply.github.com>
2023-05-12 08:25:29 -0600

    Update CORS

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ env-cmd npx functions-framework --target=gpt4
 gcloud functions deploy gpt4 \
 --allow-unauthenticated \
 --runtime=nodejs18 \
---set-env-vars OPENAI_API_KEY=PASTE_KEY_HERE \
+--update-env-vars OPENAI_API_KEY=PASTE_KEY_HERE \
+--timeout=540s \
 --trigger-http
 ```
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const functions = require('@google-cloud/functions-framework')
-const cors = require('cors')({ origin: true })
 const { Configuration, OpenAIApi } = require('openai')
 
 const configuration = new Configuration({
@@ -7,8 +6,15 @@ const configuration = new Configuration({
 })
 const openai = new OpenAIApi(configuration)
 
-functions.http('gpt4', (req, res) => {
-  cors(req, res, async () => {
+functions.http('gpt4', async (req, res) => {
+  //cloud.google.com/functions/docs/samples/functions-http-cors#functions_http_cors-nodejs
+  res.set('Access-Control-Allow-Origin', '*')
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'POST')
+    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    res.set('Access-Control-Max-Age', '3600')
+    res.status(204).send('')
+  } else {
     try {
       const completion = await openai.createChatCompletion({
         model: 'gpt-4',
@@ -18,5 +24,5 @@ functions.http('gpt4', (req, res) => {
     } catch (error) {
       res.status(500).send(error)
     }
-  })
+  }
 })
